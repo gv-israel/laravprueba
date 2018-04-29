63f58a1fbf9474740b287753d9f0093167026be7
gv-israel <38667372+gv-israel@users.noreply.github.com>
2018-04-29 20:44:24 +0200

    Add files via upload

---
+++ resources/views/plantilla/menu.blade.php
@@ -0,0 +1,7 @@
+<div class="col-sm-3 border-right">
+	<ul class="list-group">
+		<li class="list-group-item"><a href="{!!URL::to('generar-codigo')!!}">Generar Codigo</a></li>
+		<li class="list-group-item"><a href="{!!URL::to('codigos-promocionales')!!}">Mis Codigos</a></li>
+		<li class="list-group-item"><a href="{!!URL::to('cerrar-sesion')!!}" id="cerrar-sesion">Cerrar Sesion</a></li>
+	</ul>		
+</div>
